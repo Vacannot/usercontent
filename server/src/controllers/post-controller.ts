2e3c007f623cf48accdf197327942e4788fa3ae7
Vacannot <simon9916@gmail.com>
2023-06-02 17:33:15 +0200

    changes to post controller

--- server/src/controllers/post-controller.ts
+++ server/src/controllers/post-controller.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from 'express';
-import Joi from 'joi';
-import { PostModel } from '../models/post-model';
+import { NextFunction, Request, Response } from "express";
+import Joi from "joi";
+import { PostModel } from "../models/post-model";
 
 export const ensureAuthenticated = async (
   req: Request,
@@ -8,112 +8,140 @@ export const ensureAuthenticated = async (
   next: NextFunction
 ) => {
   if (!req.session?.user) {
-    res.status(401).json('Unauthorized');
+    next({ status: 401, message: "Unauthorized" });
   } else {
     next();
   }
 };
 
-export const updatePost = async (req: Request, res: Response) => {
-  const postId = req.params.id;
-  const userId = req.session!.user._id;
-  const post = await PostModel.findById(postId);
+// Permission check function
+const hasPermission = (post: any, userId: string, isAdmin: boolean) => {
+  return post.author.toString() === userId || isAdmin;
+};
 
-  if (!post) {
-    return res.status(404).json(`Post with ID ${postId} not found`);
-  }
-
-  if (
-    post.author.toString() !== userId.toString() &&
-    !req.session?.user.isAdmin
-  ) {
-    return res
-      .status(403)
-      .json(`You do not have permission to update this post`);
-  }
-
-  // Validate input
-  const schema = Joi.object({
-    title: Joi.string().trim().min(2).required(),
-    content: Joi.string().trim().min(5).required(),
-    author: Joi.string().trim().required(),
-    createdAt: Joi.string().trim().min(5).required(),
-    updatedAt: Joi.string().trim().min(5).required(),
-  });
-
-  const { _id, ...requestBody } = req.body;
-
-  const { error } = schema.validate(requestBody);
+// Validation error check function
+const validateSchema = (schema: any, payload: any, res: Response) => {
+  const { error } = schema.validate(payload);
   if (error) {
-    return res.status(400).json(error.message);
+    throw { status: 400, message: error.message };
   }
-  const updatedPost = await PostModel.findByIdAndUpdate(postId, req.body, {
-    new: true,
-  });
-  res.status(200).json(updatedPost);
 };
 
-export const deletePost = async (req: Request, res: Response) => {
-  const postId = req.params.id;
-  const userId = req.session!.user._id;
+// Update and create post schema
+const postSchema = Joi.object({
+  title: Joi.string().trim().min(2).required(),
+  content: Joi.string().trim().min(5).required(),
+});
 
-  const post = await PostModel.findById(postId);
-  if (!post) {
-    return res.status(404).json(`Post with ID ${postId} not found`);
-  }
-
-  if (
-    post.author.toString() !== userId.toString() &&
-    !req.session?.user.isAdmin
-  ) {
-    return res
-      .status(403)
-      .json(`You do not have permission to delete this post`);
-  }
-
-  await post.delete();
-
-  res.status(204).end();
-};
-
-export const createPost = async (req: Request, res: Response) => {
+export const createPost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { title, content } = req.body;
   const author = req.session!.user._id;
 
-  // Validate input
-  const schema = Joi.object({
-    title: Joi.string().trim().min(2).required(),
-    content: Joi.string().trim().min(5).required(),
-  });
+  try {
+    validateSchema(postSchema, { title, content }, res);
 
-  const { error } = schema.validate({ title, content });
-  if (error) {
-    return res.status(400).json(error.message);
+    const newPost = new PostModel({
+      title,
+      content,
+      author,
+    });
+
+    const savedPost = await newPost.save();
+    res.status(201).json(savedPost);
+  } catch (err) {
+    next(err);
   }
+};
 
-  const newPost = new PostModel({
-    title,
-    content,
-    author,
-  });
+export const updatePost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id: postId } = req.params;
+  const { user } = req.session!;
 
-  const savedPost = await newPost.save();
-  res.status(201).json(savedPost);
+  try {
+    const post = await PostModel.findById(postId);
+
+    if (!post) {
+      throw { status: 404, message: `Post with ID ${postId} not found` };
+    }
+
+    if (!hasPermission(post, user._id, user.isAdmin)) {
+      throw {
+        status: 403,
+        message: `You do not have permission to update this post`,
+      };
+    }
+
+    const { _id, ...requestBody } = req.body;
+
+    validateSchema(postSchema, requestBody, res);
+
+    const updatedPost = await PostModel.findByIdAndUpdate(postId, req.body, {
+      new: true,
+    });
+    res.status(200).json(updatedPost);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const deletePost = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id: postId } = req.params;
+  const { user } = req.session!;
+
+  try {
+    const post = await PostModel.findById(postId);
+    if (!post) {
+      throw { status: 404, message: `Post with ID ${postId} not found` };
+    }
+
+    if (!hasPermission(post, user._id, user.isAdmin)) {
+      throw {
+        status: 403,
+        message: `You do not have permission to delete this post`,
+      };
+    }
+
+    await post.delete();
+    res.status(204).end();
+  } catch (err) {
+    next(err);
+  }
 };
 
 export const getPosts = async (req: Request, res: Response) => {
-  const posts = await PostModel.find().populate('author', 'username -_id');
+  const posts = await PostModel.find().populate("author", "username -_id");
   res.json(posts);
 };
 
-export const getPostById = async (req: Request, res: Response) => {
-  const postId = req.params.id;
-  const post = await PostModel.findById(postId).populate(
-    'author',
-    'username -_id'
-  );
-  if (!post) {
-    return res.status(404).json(`Post with ID ${postId} not found`);
+export const getPostById = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id: postId } = req.params;
+
+  try {
+    const post = await PostModel.findById(postId).populate(
+      "author",
+      "username -_id"
+    );
+    if (!post) {
+      throw { status: 404, message: `Post with ID ${postId} not found` };
+    }
+    res.json(post);
+  } catch (err) {
+    next(err);
   }
-  res.json(post);
 };
